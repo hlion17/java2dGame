5ead8b85134d73f505162c5baf759d79877e748e
hlion17 <yourdad20626@gmail.com>
2023-09-17 22:30:33 +0900

    refactor: change public game panel propeties

--- src/main/java/org/example/GamePanel.java
+++ src/main/java/org/example/GamePanel.java
@@ -12,10 +12,10 @@ public class GamePanel extends JPanel implements Runnable {
     final int ORIGINAL_TITLE_SIZE = 16;  // 16x16 tile
     final int SCALE = 3;
     public final int TILE_SIZE = ORIGINAL_TITLE_SIZE * SCALE;  // 48x48 tile
-    final int MAX_SCREEN_COL = 16;
-    final int MAX_SCREEN_ROW = 12;
-    final int SCREEN_WIDTH = TILE_SIZE * MAX_SCREEN_COL;  // 768 pixels
-    final int SCREEN_HEIGHT = TILE_SIZE * MAX_SCREEN_ROW;  // 576 pixels
+    public final int MAX_SCREEN_COL = 16;
+    public final int MAX_SCREEN_ROW = 12;
+    public final int SCREEN_WIDTH = TILE_SIZE * MAX_SCREEN_COL;  // 768 pixels
+    public final int SCREEN_HEIGHT = TILE_SIZE * MAX_SCREEN_ROW;  // 576 pixels
 
     final int FPS = 60;
 
